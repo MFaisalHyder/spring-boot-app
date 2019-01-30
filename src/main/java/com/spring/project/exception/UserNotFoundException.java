5e6c667e52505db31a130c0f06635c1da25ef5fa
Muhammad Faisal Hyder <muhammadfaisal.hyder@dnata.com>
2019-01-30 11:00:21 +0400

    New method added in class

--- src/main/java/com/spring/project/exception/UserNotFoundException.java
+++ src/main/java/com/spring/project/exception/UserNotFoundException.java
@@ -2,4 +2,25 @@ package com.spring.project.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
+    /**
+     * We can add parameters in our Exception if we want to add particular messages for a given Exception instead of a
+     * single general response from ExceptionHandlerClass
+     */
+
+    private String message;
+
+    public UserNotFoundException(String message) {
+        this.message = message;
+
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
