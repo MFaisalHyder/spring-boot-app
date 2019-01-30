fce82626c9688bf468c8707aaca3c3a059d9bfe7
Muhammad Faisal Hyder <muhammadfaisal.hyder@dnata.com>
2019-01-30 10:46:23 +0400

    DTO classes added

--- src/main/java/com/spring/project/dto/BaseDTO.java
+++ src/main/java/com/spring/project/dto/BaseDTO.java
@@ -1,4 +1,26 @@
 package com.spring.project.dto;
 
-public class BaseDTO {
+import java.io.Serializable;
+
+public class BaseDTO implements Serializable {
+
+    private Long ID;
+    private String createdDate;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
 }
--- src/main/java/com/spring/project/dto/EmployeeDTO.java
+++ src/main/java/com/spring/project/dto/EmployeeDTO.java
@@ -1,4 +1,35 @@
 package com.spring.project.dto;
 
-public class EmployeeDTO {
+import java.io.Serializable;
+
+public class EmployeeDTO extends BaseDTO implements Serializable {
+
+    private String emiratesID;
+    private String firstName;
+    private String lastName;
+
+    public String getEmiratesID() {
+        return emiratesID;
+    }
+
+    public void setEmiratesID(String emiratesID) {
+        this.emiratesID = emiratesID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
