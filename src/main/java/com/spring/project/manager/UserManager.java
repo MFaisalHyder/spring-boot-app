6c1fdf99aa088ecd57c9576cc56cac531296884b
Muhammad Faisal Hyder <faisi94@gmail.com>
2019-04-14 13:00:25 +0400

    UserManager Interface Modified

--- src/main/java/com/spring/project/manager/UserManager.java
+++ src/main/java/com/spring/project/manager/UserManager.java
@@ -1,7 +1,12 @@
 package com.spring.project.manager;
 
+import com.spring.project.dto.EmployeeDTO;
 import com.spring.project.entity.Employee;
+import com.spring.project.exception.InvalidInputException;
+import com.spring.project.exception.UserNotFoundException;
+import com.spring.project.exception.UserNotRegisteredException;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -9,10 +14,14 @@ public interface UserManager {
 
     String greetUser(String name);
 
-    List<Employee> findUserByFirstName(String firstName);
+    List<EmployeeDTO> findUserByFirstName(String firstName) throws UserNotFoundException;
 
-    Employee findByEmiratesIDNumber(String emiratesIDNumber);
+    EmployeeDTO findByEmiratesIDNumber(String emiratesIDNumber) throws UserNotFoundException;
 
-    LinkedHashMap<String, Object> findAllUsers();
+    LinkedHashMap<String, Object> findAllUsers() throws UserNotFoundException;
+
+    EmployeeDTO registerUser(EmployeeDTO employee) throws UserNotRegisteredException;
+
+    EmployeeDTO loginUser(Employee employee) throws UserNotFoundException;
 
 }
