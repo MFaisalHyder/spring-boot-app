29e1bfdb897544f63e92d18f9d8733831e256bee
Muhammad Faisal Hyder <faisi94@gmail.com>
2019-04-25 11:03:56 +0400

    Code refactored

---
+++ src/main/java/com/spring/project/utility/PasswordUtil.java
@@ -0,0 +1,34 @@
+package com.spring.project.utility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class PasswordUtil {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public PasswordUtil(final BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.passwordEncoder = bCryptPasswordEncoder;
+    }
+
+    public String encryptPassword(String rawPassword) {
+
+        return passwordEncoder.encode(rawPassword);
+
+    }
+
+    public boolean doesPasswordMatch(String rawPassword, String encryptedPassword) {
+        if (!StringUtils.isEmpty(rawPassword) && !StringUtils.isEmpty(encryptedPassword)) {
+
+            return passwordEncoder.matches(rawPassword, encryptedPassword);
+        }
+
+        return false;
+
+    }
+
+}
